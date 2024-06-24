2c66ee5cf0924c7996887053cbc14544f223cef0
Pelin <kaynarcapelin@hotmail.com>
2024-06-25 01:31:35 +0300

    feat: update sidebar component

--- hotelapp/src/app/admin/layout/components/sidebar/sidebar.component.ts
+++ hotelapp/src/app/admin/layout/components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent {
-
+  constructor(public router: Router) {}
 }

3ce79034a36a74cb6da3698f1d420a7047b97c80
gurka <gurkandogan91@gmail.com>
2024-06-28 02:55:40 +0300

    refactor: add roomtypedetailcategory component,model

--- hotelapp/src/app/admin/components/room-type-detail-categories/room-type-detail-category.component.ts
+++ hotelapp/src/app/admin/components/room-type-detail-categories/room-type-detail-category.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { SweetStatus } from 'src/app/base/sweet-alert/sweet-alert-status';
+import { SweetAlertService } from 'src/app/services/admin/sweet-alert.service';
+import { RoomTypeDetailCategoryService } from 'src/app/services/common/models/room-type-detail-category.service';
+import { AddRoomTypeDetailCategory } from 'src/app/shared/models/room-type-detail-categories/add-room-type-detail-category';
+import { ListRoomTypeDetailCategory } from 'src/app/shared/models/room-type-detail-categories/list-room-type-detail-category';
+import { UpdateRoomTypeDetailCategory } from 'src/app/shared/models/room-type-detail-categories/update-room-type-detail-category';
 
 @Component({
   selector: 'app-room-type-detail-category',
@@ -6,5 +12,93 @@ import { Component } from '@angular/core';
   styleUrls: ['./room-type-detail-category.component.scss']
 })
 export class RoomTypeDetailCategoryComponent {
+  @ViewChild("roomTypeDetailCategoryForm", { static: true }) roomTypeDetailCategoryForm: NgForm
+  listRoomTypeDetailCategories: ListRoomTypeDetailCategory[] = [];
+  updateCategory: UpdateRoomTypeDetailCategory;
+  showCreateFormFlag: boolean;
+  editCategoryId: number;
+  addCategory: AddRoomTypeDetailCategory;
+
+  constructor(
+    private roomTypeDetailCategoryService: RoomTypeDetailCategoryService,
+    private sweetAlertService: SweetAlertService) { }
+
+  ngOnInit(): void {
+    this.getAll();
+  }
+
+  async getAll() {
+    return this.roomTypeDetailCategoryService.getAll().then(roomTypeDetailCategoryData => {
+      this.listRoomTypeDetailCategories = roomTypeDetailCategoryData as ListRoomTypeDetailCategory[];
+    })
+  }
+
+  showCreateForm() {
+    this.showCreateFormFlag = !this.showCreateFormFlag;
+    this.addCategory = { title: '' };
+  }
+
+  onSubmit() {
+    if (!this.roomTypeDetailCategoryForm.valid) {
+      return;
+    }
+
+    const formData = this.roomTypeDetailCategoryForm.value;
+    const RoomTypeDetailCategory: AddRoomTypeDetailCategory = {
+      title: formData.title
+    };
+
+    this.roomTypeDetailCategoryService.create(RoomTypeDetailCategory, async () => {
+      await this.sweetAlertService.showAlert(SweetStatus.sweetSucces);
+     
+    }, error => {
+    }).then(()=>{
+      this.getAll();
+      this.showCreateFormFlag = false;
+    })
+  }
+
+  showUpdateForm(roomTypeDetailCategoryId: number) {
+    const RoomTypeDetailCategoryItem = this.listRoomTypeDetailCategories.find(item => item.id === roomTypeDetailCategoryId);
+    if (RoomTypeDetailCategoryItem) {
+      this.editCategoryId = roomTypeDetailCategoryId;
+      this.updateCategory = {...RoomTypeDetailCategoryItem};
+      this.showCreateFormFlag = false;
+    }
+  }
+
+  update(roomTypeDetailCategoryId: number, action: string) {
+    if (action === 'check' && this.roomTypeDetailCategoryForm.valid && this.editCategoryId !== null) {
+      const updateRoomTypeDetailCategory: UpdateRoomTypeDetailCategory = {
+        id: roomTypeDetailCategoryId,
+        title: this.updateCategory.title
+      };
+
+      this.roomTypeDetailCategoryService.update(updateRoomTypeDetailCategory, () => {
+        this.sweetAlertService.showAlert(SweetStatus.sweetSucces);
+      },
+        error => {
+        }).then(() => {
+          this.getAll();
+          this.editCategoryId = null;
+        });
+    } else if (action === 'cancel') {
+      this.editCategoryId = null;
+    }
+    this.getAll();
+  }
+
+  async delete(categoryId: number) {
+    const sweetAlertResult = await this.sweetAlertService.showAlert(SweetStatus.deletedQuestion);
+    if (sweetAlertResult.isConfirmed) {
+      this.roomTypeDetailCategoryService.delete(categoryId, () => {
+        this.sweetAlertService.showAlert(SweetStatus.sweetSucces);
+      },
+        error => {
+        }).then(() => {
+          this.getAll();
+        });
+    }
+  }
 
 }
--- hotelapp/src/app/shared/models/room-type-detail-categories/add-room-type-detail-category.ts
+++ hotelapp/src/app/shared/models/room-type-detail-categories/add-room-type-detail-category.ts
@@ -1,4 +1,3 @@
 export class AddRoomTypeDetailCategory {
-    id: number;
     title:string;
 }

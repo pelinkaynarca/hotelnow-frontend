183e59a48484c5106a5b9a132d2152de4876d3b8
gurka <gurkandogan91@gmail.com>
2024-06-28 02:03:30 +0300

    refactor: fix comment line adminroutingmodule

--- hotelapp/src/app/admin/admin-routing.module.ts
+++ hotelapp/src/app/admin/admin-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
   {
     path: 'admin',
     component: LayoutComponent,
-    //  canActivate: [AuthGuard],
+     canActivate: [AuthGuard],
     children: [
       { path: '', component: DashboardComponent },
       {

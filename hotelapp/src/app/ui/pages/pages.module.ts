f73a235bd43a315cb91445a02c03455c0a754289
Pelin <kaynarcapelin@hotmail.com>
2024-07-01 20:15:21 +0300

    feat: add pages module in ui directory and move some components there

---
+++ hotelapp/src/app/ui/pages/pages.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ComponentsModule } from '../components/components.module';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+import { HotelDetailsComponent } from './hotel-details/hotel-details.component';
+import { HotelSearchComponent } from './hotel-search/hotel-search.component';
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent,
+    HotelDetailsComponent,
+    HotelSearchComponent,
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    ComponentsModule
+  ]
+})
+export class PagesModule { }
